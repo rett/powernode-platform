62f2aff87834fd6e05e2521e0c17d7f6244101d3
Everett C. Haimes III <everett@quickfreeze.net>
2026-02-23 20:44:42 -0900

    refactor(backend): remove marketing feature from core
    Remove marketing controllers, models, services, migrations, seeds,
    specs, factories, and worker jobs. Marketing is being extracted as
    a standalone extension.

--- server/app/models/marketing.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Marketing
-  def self.table_name_prefix
-    "marketing_"
-  end
-end
--- server/spec/factories/marketing/campaign_email_lists.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :marketing_campaign_email_list, class: "Marketing::CampaignEmailList" do
-    association :campaign, factory: :marketing_campaign
-    association :email_list, factory: :marketing_email_list
-  end
-end
