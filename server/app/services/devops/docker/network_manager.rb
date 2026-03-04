80915fec538167ba758827f6cdafb761e19d7db9
Everett C. Haimes III <everett@quickfreeze.net>
2026-03-04 10:22:29 -0900

    feat(backend): add detailed network serializer for inspect endpoint

--- server/app/services/devops/docker/network_manager.rb
+++ server/app/services/devops/docker/network_manager.rb
@@ -16,7 +16,7 @@ module Devops
       end
 
       def inspect_network(id)
-        serialize_network(@client.network_inspect(id))
+        serialize_network_detail(@client.network_inspect(id))
       rescue ApiClient::ApiError => e
         Rails.logger.error("Failed to inspect network #{id}: #{e.message}")
         raise
@@ -54,6 +54,35 @@ module Devops
           labels: data["Labels"] || {},
           created_at: data["Created"]
         }
+      end
+
+      def serialize_network_detail(data)
+        ipam = data["IPAM"] || {}
+        containers = data["Containers"] || {}
+
+        serialize_network(data).merge(
+          ipam_driver: ipam["Driver"],
+          ipam_config: (ipam["Config"] || []).map { |c|
+            {
+              subnet: c["Subnet"],
+              gateway: c["Gateway"],
+              ip_range: c["IPRange"],
+              aux_addresses: c["AuxiliaryAddresses"] || {}
+            }
+          },
+          containers: containers.map { |id, c|
+            {
+              id: id,
+              name: c["Name"],
+              ipv4_address: c["IPv4Address"] || "",
+              ipv6_address: c["IPv6Address"] || "",
+              mac_address: c["MacAddress"] || ""
+            }
+          },
+          options: data["Options"] || {},
+          enable_ipv6: data["EnableIPv6"] || false,
+          peers: (data["Peers"] || []).map { |p| { name: p["Name"], ip: p["IP"] } }
+        )
       end
     end
   end
